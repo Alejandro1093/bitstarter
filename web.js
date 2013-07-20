812fafb734819f99ccc169118789b040d5f64805
Alejandro1093 <j.alejandro_1093@hotmail.com>
2013-07-20 00:48:35 +0000

    another update on web.js

--- web.js
+++ web.js
@@ -6,15 +6,13 @@ var fs = require('fs');
 
 app.get('/', function(request, response) {
 
-    fs.readFile('/home/ubuntu/bitstarter/index.html', 'utf8', function (err,data){
+    fs.readFile('./index.html', 'utf8', function (err,data){
 	if(err) {
 	    return console.log(err);
 	    }
-
-	});
-
-});
-
+	console.log(data);
+    });
+    
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
