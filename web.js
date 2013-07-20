72cc446a30e332077dbe2d0af4ad544385287747
Alejandro1093 <j.alejandro_1093@hotmail.com>
2013-07-20 00:19:28 +0000

    another update on web.js

--- web.js
+++ web.js
@@ -17,8 +17,9 @@ fs.readFile('/bitstarter/index.html', 'utf8', function (err, data) {
 
 
 app.get('/', function(request, response) {
-  response.send(datas);
+  response.send('wassa');
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
